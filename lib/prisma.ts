07cd18167f241f4fe6f07fcc8e5aed0b4ccce7ae
shubham12-prof <shubhsemwal12@gmail.com>
2026-04-27 23:49:32 +0530

    vercel changes build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,11 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require("@prisma/client");
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  prisma: any | undefined;
 };
 
 const adapter = new PrismaPg({
